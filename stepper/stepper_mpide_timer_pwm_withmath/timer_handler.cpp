0b7f3f8a64731e8c11ddf9c1670a339429c6f63f
1i7 <ii1i7@yandex.ru>
2014-04-27 13:03:16 +0400

    Незначительные исправления

--- stepper/stepper_mpide_timer_pwm_withmath/timer_handler.cpp
+++ stepper/stepper_mpide_timer_pwm_withmath/timer_handler.cpp
@@ -23,6 +23,10 @@ int motor2_pin_en;
 int motor1_pulse_delay = 500;
 int motor2_pulse_delay = 500;
 
+// Инверсия направления на ножке dir: 1 - не инвертировать направленение, -1 - инвертировать.
+int motor1_dir_inv;
+int motor2_dir_inv;
+
 //// Настройки для текущего цикла шагов.
 
 // Количество шагов в текущей серии
@@ -42,10 +46,6 @@ int motor2_step_counter;
 int motor1_step_timer;
 int motor2_step_timer;
 
-// Инверсия направления на ножке dir: 1 - не инвертировать направленение, -1 - инвертировать.
-int motor1_dir_inv;
-int motor2_dir_inv;
-
 // Частота таймера, мкс
 int timer_freq_us;
 
@@ -60,14 +60,13 @@ void init_motor1(int pin_pulse, int pin_dir, int pin_en, int pulse_delay, int di
     motor1_pin_dir = pin_dir;
     motor1_pin_en = pin_en;
     motor1_pulse_delay = pulse_delay;
+    motor1_dir_inv = dir_inv;
     
     // сбросить все динамические настройки
     motor1_step_counter = 0;
     motor1_step_delay = 0;
     motor1_step_timer = 0;
-    
-    motor1_dir_inv = dir_inv;
-    
+        
     // задать настройки пинов
     pinMode(motor1_pin_pulse, OUTPUT);
     pinMode(motor1_pin_dir, OUTPUT);
@@ -85,14 +84,13 @@ void init_motor2(int pin_pulse, int pin_dir, int pin_en, int pulse_delay, int di
     motor2_pin_dir = pin_dir;
     motor2_pin_en = pin_en;
     motor2_pulse_delay = pulse_delay;
+    motor2_dir_inv = dir_inv;
     
     // сбросить все динамические настройки
     motor2_step_counter = 0;
     motor2_step_delay = 0;
     motor2_step_timer = 0;
-    
-    motor2_dir_inv = dir_inv;
-    
+        
     // задать настройки пинов
     pinMode(motor2_pin_pulse, OUTPUT);
     pinMode(motor2_pin_dir, OUTPUT);
@@ -207,13 +205,13 @@ void handle_interrupts(int timer) {
     }
     
     if(motor2_step_counter > 0) {
-        if(motor2_step_timer < motor2_pulse_delay + timer_freq_us && motor2_step_timer >= motor1_pulse_delay) {
+        if(motor2_step_timer < motor2_pulse_delay + timer_freq_us && motor2_step_timer >= motor2_pulse_delay) {
             // motor2_step_timer ~ motor2_pulse_delay с учетом погрешности таймера timer_freq_us =>
             // импульс1 - готовим шаг
             digitalWrite(motor2_pin_pulse, HIGH);
         } else if(motor2_step_timer < timer_freq_us) {
-            // motor1_step_timer ~ 0 с учетом погрешности таймера (timer_freq_us) =>
-            // импульс2 (спустя motor1_pulse_delay микросекунд после импульса1) - завершаем шаг
+            // motor2_step_timer ~ 0 с учетом погрешности таймера (timer_freq_us) =>
+            // импульс2 (спустя motor2_pulse_delay микросекунд после импульса1) - завершаем шаг
             digitalWrite(motor2_pin_pulse, LOW);
             
             // переустановим таймер
