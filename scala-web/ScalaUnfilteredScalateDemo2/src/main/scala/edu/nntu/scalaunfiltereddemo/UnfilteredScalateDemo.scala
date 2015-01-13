3c4b70ace1318cb59009181a7485f88b0a6f98ed
1i7 <ii1i7@yandex.ru>
2015-01-13 19:43:19 +0300

    Примеры работы со Scalate в Unfiltered

---
+++ scala-web/ScalaUnfilteredScalateDemo2/src/main/scala/edu/nntu/scalaunfiltereddemo/UnfilteredScalateDemo.scala
@@ -0,0 +1,30 @@
+package edu.nntu.scalaunfiltereddemo
+
+import unfiltered.request.Path
+import unfiltered.request.Seg
+import unfiltered.request.GET
+import unfiltered.response.Ok
+import unfiltered.scalate.Scalate
+
+/**
+ * http://www.slideshare.net/strachaj/introducing-scalate-the-scala-template-engine
+ * http://scalate.github.io/scalate/documentation/ssp-reference.html
+ * 
+ * Scalate+Unfiltered:
+ * https://github.com/unfiltered/unfiltered-scalate.g8/blob/master/src/main/g8/src/main/scala/Example.scala
+ * 
+ */
+object UnfilteredScalateDemo {
+
+  val handlePath = unfiltered.filter.Planify {
+    case req@GET(Path(Seg(Nil))) =>
+      Ok ~> Scalate(req, "index.ssp")
+  }
+
+  def main(args: Array[String]) {
+    println("Starting jetty http server demo...")
+
+    // Запустить веб-сервер
+    unfiltered.jetty.Http.apply(8080).filter(handlePath).run()
+  }
+}
