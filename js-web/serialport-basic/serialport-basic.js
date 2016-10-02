de52c865dae33819c4faf8fcac65b2c8099b3917
1i7 <ii1i7@yandex.ru>
2016-10-02 22:46:01 +0300

    Пример работыс последовательным портом (и Ардуиной)

---
+++ js-web/serialport-basic/serialport-basic.js
@@ -0,0 +1,93 @@
+// https://github.com/EmergingTechnologyAdvisors/node-serialport#usage
+var SerialPort = require('serialport');
+
+port = new SerialPort("/dev/ttyUSB0", {
+    // скорость
+    baudRate: 9600,
+    //baudRate: 115200,
+    // получать данные по одной строке
+    parser: SerialPort.parsers.readline('\n'),
+    // не открывать порт сразу здесь
+    autoOpen: false,
+    lock: true
+});
+
+function writeData() {
+    data = "ping";
+    console.log("write: " + data);
+    port.write(data,
+        function(err) {
+            if(!err) {
+                // данные ушли ок
+                console.log("write ok");
+            } else {
+                // ошибка записи в порт 
+                // (например, порт открыт, но не хватает прав на запись)
+                console.log("write error: " + err);
+            }
+        }
+    );
+    
+    // после первого вызова повторять бесконечно в цикле
+    //setTimeout(writeData, 1000);
+}
+
+// 
+// События
+// 
+
+// порт открылся
+port.on('open', function () {
+    console.log("open");
+    
+    // Weird behaviour when send packets just after port is open
+    // http://chipkit.net/forum/viewtopic.php?f=19&t=3731
+    
+    // Arduino sketch will start to work after the 
+    // last writeData is executed
+    // Скетч Ардуино начнет работать только после того,
+    // как отработает последний вызов writeData
+    //writeData();
+    /*setTimeout(writeData, 3000);
+    setTimeout(writeData, 6000);
+    setTimeout(writeData, 9000);
+    setTimeout(writeData, 12000);
+    */
+    
+    // Arduino sketch will never start to work this way
+    // так скетч Ардуино никогда не загрузится
+    setInterval(writeData, 2000);
+    
+    // the only way to make Arduino sketch to work
+    // единственный способ дать прошивке Ардуины загрузиться до конца - 
+    // отправлять первое сообщение не раньше, чем через 5 секунд
+    // после открытия порта платы (точнее, между двумя отправками сообщения
+    // до загрузки скетча должно пройти как минимум 5 секунд)
+    //setInterval(writeData, 5000);
+});
+
+// пришли данные
+port.on('data', function(data) {
+    console.log("data: " + data);
+});
+
+// отключили устройство (выдернули провод)
+port.on('disconnect', function () {
+    console.log("disconnect");
+});
+
+// 
+// Действия
+//
+
+// открываем порт
+port.open(function(err) {
+    if(err) {
+        // не получилось открыть порт
+        console.log("Error opening: " + err);
+    } else {
+        console.log("Open OK");
+        //setInterval(writeData, 1000);
+    }
+});
+
