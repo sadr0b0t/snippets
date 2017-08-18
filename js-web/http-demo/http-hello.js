a65a6c347d168d2aca952642d41cdc25ce4229e0
1i7 <ii1i7@yandex.ru>
2017-08-18 19:00:40 +0300

    Простейший сервер http на ноде

---
+++ js-web/http-demo/http-hello.js
@@ -0,0 +1,13 @@
+var http = require('http');
+ 
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  var a=3;
+  var b=5;
+  response.write('' + a + ' + ' + b + ' = ' + (a+b) + '\n');
+  
+  response.end('работает\n');
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
+
